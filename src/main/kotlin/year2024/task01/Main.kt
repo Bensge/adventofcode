78cf669408ea2105fde47801e382ce6a716b39d3
Benno <benno@bensge.com>
2024-12-01 14:53:28 +0100

    Exercise 1.2

--- src/main/kotlin/year2024/task01/Main.kt
+++ src/main/kotlin/year2024/task01/Main.kt
@@ -5,24 +5,43 @@ import java.lang.Integer.parseInt
 fun getResourceAsText(path: String): String? =
     object {}::class.java.classLoader.getResource(path)?.readText()
 
-fun main() {
-    val input = getResourceAsText("01.txt")!!
+typealias Data = Pair<MutableList<Int>, MutableList<Int>>
 
-    val sum = input.lineSequence()
+fun loadData(): Data =
+    getResourceAsText("01.txt")!!
+        .lineSequence()
         .map { it.split("   ", limit = 2) }
         .map { Pair(parseInt(it[0]), parseInt(it[1])) }
-        .fold(
-            Pair(mutableListOf<Int>(), mutableListOf<Int>())) {
-            lists, values ->
-                lists.first.add(values.first)
-                lists.second.add(values.second)
-                lists
+        .fold(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { lists, values ->
+            lists.first.add(values.first)
+            lists.second.add(values.second)
+            lists
         }
+
+fun partOne(input: Data) =
+    input
         .let { Pair(it.first.sorted(), it.second.sorted()) }
         .let {
             it.first.zip(it.second).map { Math.abs(it.first - it.second) }
         }
         .sum()
 
-    print(sum)
+fun partTwo(input: Data): Int {
+    val histogram = input.second.fold(mutableMapOf<Int, Int>()) { acc, value ->
+        acc[value] = 1 + acc.getOrDefault(value, 0)
+        acc
+    }
+    return input.first.sumOf { it*histogram.getOrDefault(it, 0) }
+}
+
+
+fun main() {
+    loadData().let { data ->
+        partOne(data).also {
+            println("part 1 solution = ${it}")
+        }
+        partTwo(data).also {
+            println("part 2 solution = ${it}")
+        }
+    }
 }
